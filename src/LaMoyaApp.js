a48a06aacdcdfcea046da57ae884bf64a5fc5692
DavidPaca <david.paca1992@gmail.com>
2022-08-26 21:20:20 -0500

    iniciocorregido1

--- src/LaMoyaApp.js
+++ src/LaMoyaApp.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import { GastronomiaScreen } from './components/initialPages/GastronomiaScreen'
 import { IndexScreen } from './components/initialPages/IndexScreen'
+import { IndexPack1Screen } from './components/initialPages/IndexPack1Screen'
+import { IndexPack2Screen } from './components/initialPages/IndexPack2Screen'
 export const LaMoyaApp = () => {
   return (
 
@@ -9,6 +11,8 @@ export const LaMoyaApp = () => {
         <Routes>
           <Route path="/" element={<IndexScreen/>}/>
           <Route path="gastronomia" element={<GastronomiaScreen/>}/>
+          <Route path="pack1" element={<IndexPack1Screen/>}/>
+          <Route path="pack2" element={<IndexPack2Screen/>}/>
         </Routes>
       </BrowserRouter>
     )
