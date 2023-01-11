34b3f252ca1898b4ec8c941ecc2a522151e7ad9e
darday <darday1980@gmail.com>
2023-01-11 04:35:53 -0500

    Rutas creadas

--- src/LaMoyaApp.js
+++ src/LaMoyaApp.js
@@ -9,6 +9,10 @@ import { CuyesScreen } from './components/initialPages/CuyesScreen'
 import { ProyectosScreen } from './components/initialPages/ProyectosScreen'
 import { GaleriaScreen } from './components/gallery/GaleriaScreen'
 import { EconomyScreen } from './components/initialPages/EconomyScreen'
+import { Project1Screen } from './components/initialPages/projects/Project1Screen'
+import { Project2Screen } from './components/initialPages/projects/Project2Screen'
+import { Project3Screen } from './components/initialPages/projects/Project3Screen'
+import { Project4Screen } from './components/initialPages/projects/Project4Screen'
 export const LaMoyaApp = () => {
   return (
 
@@ -21,8 +25,10 @@ export const LaMoyaApp = () => {
           <Route path="proyectos" element={<ProyectosScreen/>}/>
           <Route path="galeria" element={<GaleriaScreen/>}/>
           <Route path="contactos" element={<ContactosScreen/>}/>
-          <Route path="pack1" element={<IndexPack1Screen/>}/>
-          <Route path="pack2" element={<IndexPack2Screen/>}/>
+          <Route path="proyecto1" element={<Project1Screen/>}/>
+          <Route path="proyecto2" element={<Project2Screen/>}/>
+          <Route path="proyecto3" element={<Project3Screen/>}/>
+          <Route path="proyecto4" element={<Project4Screen/>}/>
         </Routes>
       </BrowserRouter>
     )
